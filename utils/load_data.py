da4071df8247086480af23e981fe6735eaf4b3f1
mysakashd-png <mys.akash.d@gmail.com>
2026-06-04 10:42:37 +0530

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import streamlit as st
 
+@st.cache_data
+def load_data(path="data/startup_data.csv"):
+    df = pd.read_csv(path)
+
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
+
+    # Handle missing values
+    df.fillna(0, inplace=True)
+
+    return df
