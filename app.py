a523d032ed3443659de373de6394f91ee2f51966
mysakashd-png <mys.akash.d@gmail.com>
2026-06-04 15:01:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,9 +49,9 @@ logo_path = "assets/vtulogo.png"
 # Sidebar
 if os.path.exists(logo_path):
     try:
-        st.sidebar.image(logo_path, width=880)
+        st.sidebar.image(logo_path, width=180)
     except Exception:
-        st.sidebar.markdown("### 🚀 Startup Analytics Dashboard")
+       # st.sidebar.markdown("### 🚀 Startup Analytics Dashboard")
 
 # Sidebar Navigation
 st.sidebar.title("Navigation")
