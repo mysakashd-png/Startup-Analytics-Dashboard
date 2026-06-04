05d41aedd03a59ffc6e8b67950489335fd89613f
mysakashd-png <mys.akash.d@gmail.com>
2026-06-04 15:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,15 +46,6 @@ df = load_data()
 # -----------------------------
 logo_path = "assets/vtulogo.png"
 
-# Sidebar
-if os.path.exists(logo_path):
-    try:
-        pass
-        print("hello")
-       # st.sidebar.image(logo_path, width=180)
-    except Exception:
-        st.sidebar.markdown("### 🚀 Startup Analytics Dashboard")
-
 # Sidebar Navigation
 st.sidebar.title("Navigation")
 
